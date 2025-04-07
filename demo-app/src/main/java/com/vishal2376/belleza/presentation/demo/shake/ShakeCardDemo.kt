6b3bb29a2d410528fad9c9e35aa5798345d6cfdc
Vishal Singh <vishalsingh2376@gmail.com>
2025-04-08 02:11:17 +0530

    feat: add ShakeCardDemo screen to showcase vertical shake animation

--- demo-app/src/main/java/com/vishal2376/belleza/presentation/demo/shake/ShakeCardDemo.kt
+++ demo-app/src/main/java/com/vishal2376/belleza/presentation/demo/shake/ShakeCardDemo.kt
@@ -1,16 +1,8 @@
 package com.vishal2376.belleza.presentation.demo.shake
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.vishal2376.belleza.presentation.app.BellezaDemo
-import com.vishal2376.belleza.shake.ShakeCard
-import com.vishal2376.belleza.shake.ShakeIntensity
-import com.vishal2376.belleza.shake.rememberShakeCardState
+import com.vishal2376.belleza.presentation.components.BellezaScreen
 
 object ShakeCardDemo : BellezaDemo {
 	override val title: String
@@ -19,17 +11,13 @@ object ShakeCardDemo : BellezaDemo {
 		get() = "shake"
 
 	@Composable
-	override fun Screen() {
-		val state = rememberShakeCardState(shake = true)
-
-		Column(
-			modifier = Modifier.fillMaxSize(),
-			verticalArrangement = Arrangement.Center,
-			horizontalAlignment = Alignment.CenterHorizontally
+	override fun Screen(onClickBack: () -> Unit) {
+		BellezaScreen(
+			title = title,
+			showBack = true,
+			onClickBack = onClickBack
 		) {
-			ShakeCard(state = state, intensity = ShakeIntensity.HIGH) {
-				Text("I'm shaking ✨")
-			}
+			ShakeCardDemoScreen()
 		}
 	}
 }
