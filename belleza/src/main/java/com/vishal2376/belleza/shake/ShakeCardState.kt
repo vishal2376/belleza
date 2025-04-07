6045183b4bf973f70747f1d4b17c56e8531fc5b5
Vishal Singh <vishalsingh2376@gmail.com>
2025-04-08 02:10:03 +0530

    feat: add ShakeCardState with reactive shake and direction state

--- belleza/src/main/java/com/vishal2376/belleza/shake/ShakeCardState.kt
+++ belleza/src/main/java/com/vishal2376/belleza/shake/ShakeCardState.kt
@@ -1,2 +1,23 @@
-package com.vishal2376.belleza
+package com.vishal2376.belleza.shake
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+class ShakeCardState(
+	shake: Boolean,
+	shakeDirection: ShakeDirection
+) {
+	var shake by mutableStateOf(shake)
+	var shakeDirection by mutableStateOf(shakeDirection)
+}
+
+@Composable
+fun rememberShakeCardState(
+	shake: Boolean = false,
+	shakeDirection: ShakeDirection = ShakeDefaults.Direction
+): ShakeCardState = remember {
+	ShakeCardState(shake, shakeDirection)
+}
